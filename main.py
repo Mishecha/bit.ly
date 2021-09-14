1564af649f2c6463580fb05685491d4078157168
Mishecha <laptevm472@gmail.com>
2021-09-14 10:59:18 +0200

    добавил пустые строки

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 import argparse
 
+
 def shorten_link(headers, payload):
   shorten_url = 'https://api-ssl.bitly.com/v4/shorten'
   response = requests.post(shorten_url, headers=headers, json=payload)
@@ -34,11 +35,13 @@ def get_parsed_user_link(user_link):
 if __name__ == '__main__':
   load_dotenv()
 
+
   parser = argparse.ArgumentParser(
     description='Сокращение ссылок'
   )
   parser.add_argument('user_link', help='')
   user_link = parser.parse_args().user_link
+
 
   bitly_token = os.environ['BITLY_TOKEN']
   netloc_and_path = get_parsed_user_link(user_link)
